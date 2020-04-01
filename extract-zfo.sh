85003dc916e321201575e2e35be42f96e1c76f38
vit-zikmnund <vit_zikmund@cz.ibm.com>
2020-04-01 12:08:14 +0200

    fix(extract-zfo): allow for whitespace in attachment names

--- extract-zfo.sh
+++ extract-zfo.sh
@@ -1,21 +1,67 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
-input="$1"
-xml="$input.xml"
-openssl smime -inform DER -in "$input" -verify -noverify -out "$xml"
-trap 'rm -f "$xml"' EXIT
-xml="$(realpath "$xml")"
-files=$(xmllint --xpath "string(//*[local-name()='dmFile']/@dmFileDescr)" "$xml")
-if [ -z "$files" ]; then
-  echo "No files found!"; exit 1
-fi
-echo "Found the following files: $files"
-outdir="$(basename "$input")"
-outdir="${outdir%.*}"
-echo "Extracting them to: $(pwd)/$outdir"
-mkdir -p "$outdir"
-cd "$outdir"
-for file in $files; do
-    echo "Extracting $file"
-    xmllint --xpath "//*[local-name()='dmFile' and @dmFileDescr='$file']/*[local-name()='dmEncodedContent']/text()" "$xml" | base64 -d > "$file"
-done
+
+main() {
+  local infile infile_extension
+  infile="$1"
+  infile_extension="${infile##*.}"
+  OUTDIR="$(realpath "$infile").files"
+
+  if [[ "$infile_extension" == 'zfo' ]]; then
+    extract_zfo "$infile"
+  else
+    err 1 "error: unsupported extension (use .zfo)\n" || return $?
+  fi
+}
+
+extract_zfo() {
+  local xml rc=0 stylesheet files
+
+  log "info: verifying the document signature (openssl): "
+  xml="$(openssl smime -inform DER -verify -noverify -in "$1")" || rc=$?
+  [[ "$rc" -ne 0 ]] && return "$rc"
+
+  stylesheet='
+    <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+      <xsl:output omit-xml-declaration="yes"/>
+      <xsl:template match="/">
+        <xsl:for-each select="'"//*[local-name()='dmFile']"'">
+          <xsl:value-of select="@dmFileDescr"/>
+          <xsl:text>&#xa;</xsl:text>
+        </xsl:for-each>
+      </xsl:template>
+    </xsl:stylesheet>'
+  files="$(xsltproc <(printf '%s' "$stylesheet") - <<<"$xml")"
+
+  if [ -z "$files" ]; then
+    err 1 "error: no files found\n" || return $?
+  fi
+  log "info: extracting files to: %s\n" "$(basename "$OUTDIR")"
+  mkdir -p "$OUTDIR"
+  cd "$OUTDIR"
+
+  while read -r file; do
+    stylesheet='
+      <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
+        <xsl:output omit-xml-declaration="yes"/>
+        <xsl:template match="/">
+          <xsl:value-of select="'"//*[local-name()='dmFile' and @dmFileDescr='$file']/*[local-name()='dmEncodedContent']/text()"'"/>
+        </xsl:template>
+      </xsl:stylesheet>'
+    log "info: extracting %s\n" "$file"
+    xsltproc <(printf '%s' "$stylesheet") - <<<"$xml" | base64 -d > "$file"
+  done <<<"$files"
+}
+
+log() {
+  printf "$@" >&2
+}
+
+err() {
+  local e="$1"
+  shift
+  log "$@"
+  return "$e"
+}
+
+main "$@"
